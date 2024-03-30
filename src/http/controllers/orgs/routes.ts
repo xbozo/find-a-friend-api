eb84287332b7d45f2fc0d86fa2c7da1a5013a739
Guilherme Viana <imluizguilara@hotmail.com>
2024-03-30 12:58:11 -0300

    feat: create org refresh token

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { authenticateController } from './authenticate-controller'
 import { createOrgController } from './create-org-controller'
+import { refreshController } from './refresh-controller'
 
 export const orgsRoutes = async (app: FastifyInstance) => {
 	app.post('/orgs', createOrgController)
 	app.post('/sessions', authenticateController)
+
+	app.patch('/token/refresh', refreshController)
 }
